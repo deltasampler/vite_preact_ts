270cdeb79714a76572f1867bec5e5ecb301e6e6b
deltasampler <deltasampler@gmail.com>
2025-04-12 14:32:48 +0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import path from "path";
+import {defineConfig} from "vite";
+import preact from "@preact/preset-vite";
+
+export default defineConfig({
+    plugins: [preact()],
+    resolve: {
+        alias: {}
+    }
+});
